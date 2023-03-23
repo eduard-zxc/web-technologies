9db7bee9e1a521482757d5ec605a0b98b8ac6811
dogotariii <ianec.dogotari.isa.utm.md>
2023-03-23 15:51:32 +0200

    Addded business logic

---
+++ eUseControl.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using eUseControl.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            UserData u = new UserData
+            {
+                Username = "customer",
+                Products = new List<string> { "Product #1", "Product #2", "Product #3" }
+            };
+
+            return View(u);
+        }
+
+        public ActionResult Classes()
+        {
+            var product = Request.QueryString["p"];
+
+            UserData u = new UserData();
+            u.Username= "customer";
+            u.SingleProduct = product;
+
+            return View(u);
+        }
+
+        [HttpPost]
+        public ActionResult Classes(string btn)
+        {
+            return RedirectToAction("Classes", "Home", new { @p = btn });
+        }
+    }
+}
