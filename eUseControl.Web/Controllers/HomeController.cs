72977c6f1f90079875c7ecddf6212f4c5cce0797
Dorin <dorin.rosca@isa.utm.md>
2023-03-23 16:16:10 +0200

    Added redirect buttons

---
+++ eUseControl.Web/Controllers/HomeController.cs
@@ -0,0 +1,40 @@
+﻿using eUseControl.Web.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace eUseControl.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            UserData u = new UserData();
+            u.Username = "Costumer";
+            u.Products = new List<string> { "Product #1", "Product #2", "Product #3", "Product #4" };
+           
+            return View(u);
+        }
+
+
+
+        public ActionResult Product()
+        {
+            var product = Request.QueryString["p"];
+
+            UserData u = new UserData();
+            u.Username = "Costumer";
+            u.SingleProduct = product;
+
+            return View(u);
+        }
+        [HttpPost]
+        public ActionResult Product(string btn)
+        {
+            return RedirectToAction("Product", "Home", new { @p = btn });
+        }
+    }
+}
